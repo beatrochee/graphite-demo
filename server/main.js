7e319e7372d732a71f6faffbee707f02e4afaa02
beatrochee <jrb514github@gmail.com>
2022-02-01 17:54:23 +0100

    Add Request Processor

--- server/main.js
+++ server/main.js
@@ -1,3 +1,7 @@
 console.log("i am a server, beep boop")
 console.log("this is another part of the server that should've been committed with the infrastructure, dangit!")
 console.log("i'm on the backend team and I am doing some server work!");
+
+function processRequest(req) {
+    console.log('I am processing!')
+}
