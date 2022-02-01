bacd880af0359dc2ea3a6c4e96f6ae36d0df6b2f
beatrochee <jrb514github@gmail.com>
2022-02-01 17:27:53 +0100

    doing server work

--- server/main.js
+++ server/main.js
@@ -1,2 +1,3 @@
 console.log("i am a server, beep boop")
 console.log("this is another part of the server that should've been committed with the infrastructure, dangit!")
+console.log("i'm on the backend team and I am doing some server work!");
